c8e17e4f4c60c2b30d4e158ded0ad870e7beeea5
Spelchure <alperenbayramoglu2@gmail.com>
2023-06-03 11:44:21 +0300

    fix(nvim): update nvim tree on_attach fn

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -8,9 +8,18 @@ if not config_status_ok then
   return
 end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api");
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  -- vim.keymap.set
+end
 
 nvim_tree.setup {
+  on_attach = on_attach,
   update_focused_file = {
     enable = true,
     update_cwd = true,
@@ -59,12 +68,5 @@ nvim_tree.setup {
   view = {
     width = 30,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
   },
 }
